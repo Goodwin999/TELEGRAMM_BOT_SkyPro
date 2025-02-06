aed05ab3500cb175f83d66fe189931b7028cc375
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-02-07 01:26:07 +0300

    Шаг 2.4 выполнен (в свагере не открывается приложение)

--- src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class NotificationTaskService {
@@ -16,6 +20,27 @@ public class NotificationTaskService {
     @Autowired
     public NotificationTaskService(NotificationTaskRepository notificationTaskRepository) {
         this.notificationTaskRepository = notificationTaskRepository;
+    }
+
+    public String parseAndSaveNotification(String message) {
+        Pattern pattern = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4}\\s\\d{2}:\\d{2})(\\s+)(.+)");
+        Matcher matcher = pattern.matcher(message);
+
+        if (matcher.find()) {
+            String dateTimeStr = matcher.group(1);
+            String notificationMessage = matcher.group(3);
+
+            LocalDateTime notificationDateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+
+            NotificationTask notificationTask = new NotificationTask();
+            notificationTask.setNotificationDateTime(notificationDateTime);
+            notificationTask.setMessage(notificationMessage);
+
+            notificationTaskRepository.save(notificationTask);
+            return "Задача успешно добавлена!";
+        } else {
+            throw new IllegalArgumentException("Неверный формат сообщения!");
+        }
     }
 
     public NotificationTask saveNotificationTask(NotificationTask notificationTask) {

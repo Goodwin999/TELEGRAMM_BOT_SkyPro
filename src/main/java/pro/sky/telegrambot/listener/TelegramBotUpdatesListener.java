6027b78bcf2e98f4a57c79257c3eafcc605598bc
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2025-02-09 00:21:49 +0300

    Создан интерфейс который реализуется сервисом, написано приветственное сообщение, обработка реализована, ответ приходит в чат, в консоли имеются читаемые логи, продолжаем;

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,20 +3,30 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import pro.sky.telegrambot.service.MessageSender;
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    @Autowired
-    private TelegramBot telegramBot;
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final MessageSender messageSender;
+    private final TelegramBot telegramBot;
+    private final String WELCOME_MESSAGE = "Привет Меченый! Короче, я тебя спас и в благородства играть не буду, выполни для меня пару заданий, и мы в расчете! ";
+    private final Pattern INCOMING_MESSAGE_PATTERN = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{4}\\s\\d{2}:\\d{2})(\\s+)(.+)");
+
+
+
+    public TelegramBotUpdatesListener(MessageSender messageSender, TelegramBot telegramBot) {
+        this.messageSender = messageSender;
+        this.telegramBot = telegramBot;
+    }
+
+
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
@@ -26,11 +36,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Обновление обработки: {}", update);
-            // Проверяем, является ли текст сообщения командой /start
-            if (update.message() != null && update.message().text().equals("/start")) {
-                long chatId = update.message().chat().id();  // Получаем идентификатор чата
-                // Отправляем приветственное сообщение в этот чат
-                telegramBot.execute(new SendMessage(chatId, "А я думал это сова!"));
+
+            long chatId = update.message().chat().id();
+            String message = update.message().text();
+
+            if ("/start".equals(message)) {
+                messageSender.send(chatId, WELCOME_MESSAGE);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
---
+++ src/main/java/pro/sky/telegrambot/service/MessageSenderImpl.java
@@ -0,0 +1,36 @@
+package pro.sky.telegrambot.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSenderImpl implements MessageSender {
+    private final Logger logger = LoggerFactory.getLogger(MessageSenderImpl.class);
+
+    private final TelegramBot telegramBot;
+
+    public MessageSenderImpl(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @Override
+    public void send(long chatId, String messageText) {
+        SendMessage message = new SendMessage(chatId, messageText);
+
+        SendResponse response = telegramBot.execute(message);
+        if (response.isOk()) {
+            logger.info("Сообщение для ChatId {} и текстовое сообщение {} успешно отправлены.", chatId, message);
+        } else {
+logger.error("Произошла ошибка при отправке сообщения для ChatId {} и текстовое сообщение {}", chatId, message);
+
+
+        }
+    }
+
+
+    }
+
